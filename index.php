46906ebfa4ca5f30a60a117f93608ae079b07104
Romain <choppertapapeur@gmail.com>
2019-01-09 23:43:37 +0000

    première étape construction de demande.xml

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
+<?php
+#quand on a submit quelque chose on prépare notre fichier xml
+if(!empty($_POST))
+{
+	# nécessite le paquet php-xml
+	# aussi chown www-data demande.xml
+	$xml = new DOMDocument("1.0","UTF-8");
+	$xml->load("demande.xml");
+
+	$rootTag = $xml->getElementsByTagName("document")->item(0);
+
+	#suppression de l'information précédente
+	while($rootTag->hasChildNodes())
+	{
+		$rootTag->removeChild($rootTag->firstChild);
+	}
+	if(isset($_REQUEST['create']))
+	{
+		$nb = $_REQUEST['nb'];
+		$image = $_REQUEST['image'];
+
+		$dataTag = $xml->createElement("data");
+
+		$actionTag = $xml->createElement("action","create");
+		$imageTag = $xml->createElement("image",$image);
+		$nbTag = $xml->createElement("nb",$nb);
+
+		$dataTag->appendChild($actionTag);
+		$dataTag->appendChild($imageTag);
+		$dataTag->appendChild($nbTag);
+
+		$rootTag->appendChild($dataTag);
+
+		$xml->save("demande.xml");
+		echo "Création de $nb conteneur(s) $image.";
+	}
+
+}
+
+#on met tout le html dans une variable que l'on echo
+$html='
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Page de gestion</title>
 	</head>
-<?php
-if(isset($_REQUEST['create']))
-{
-	$nb = $_REQUEST['nb'];
-	echo "création de $nb conteneur(s).";
-}
-?>
 	<body>
 		<form action="index.php" method="post">
 			<select name="image">
@@ -34,3 +68,5 @@ if(isset($_REQUEST['create']))
 		</form>
 	</body>
 </html>
+';
+echo $html;
